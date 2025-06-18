1d46a4155ab14f4f19a707419c7dbab30e80c81c
Tobias <t.hc199811@gmail.com>
2025-06-18 10:23:02 +0200

    feat: parseDuration function from golang ported to ts

--- web/src/lib/types.ts
+++ web/src/lib/types.ts
@@ -5,3 +5,23 @@ export interface Session {
 	createdAt: string;
 	updatedAt: string;
 }
+
+
+export interface RegisterTimeEntryInput {
+	categoryId: number;
+	date: string;
+	duration: string;
+	description: string;
+}
+
+export interface TimeEntry {
+	id: number;
+	categoryId: number;
+	category: string;
+	userId: number;
+	date: string;
+	duration: string;
+	description: string;
+}
+
+export type Duration = number
