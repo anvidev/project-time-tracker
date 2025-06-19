5a002b993d4e16a7e04cc10c607cb9857dcfd829
Tobias <t.hc199811@gmail.com>
2025-06-19 15:36:22 +0200

    wip: ui registration ui

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -4,9 +4,12 @@ import { redirect, type Handle } from '@sveltejs/kit';
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.apiService = ApiServiceFactory(event.fetch, 'http://localhost:9090');
 
-	if (!event.url.pathname.startsWith('/auth') && event.cookies.get('authToken') == undefined) {
+	const authToken = event.cookies.get('authToken')
+	if (!event.url.pathname.startsWith('/auth') && authToken == undefined) {
 		redirect(303, '/auth/login')
 	}
+
+	event.locals.authToken = authToken ?? ""
 
 	return await resolve(event);
 };
--- web/src/routes/(site)/+page.svelte
+++ web/src/routes/(site)/+page.svelte
@@ -4,10 +4,15 @@
 	import { Label } from '$lib/components/ui/label';
 	import DatePicker from '$lib/components/ui/button/date-picker/DatePicker.svelte';
 	import { Button } from '$lib/components/ui/button';
-	import { fromDate, getLocalTimeZone, type DateValue } from '@internationalized/date';
+	import {
+		DateFormatter,
+		fromDate,
+		getLocalTimeZone,
+		type DateValue
+	} from '@internationalized/date';
 	import { format, toDate } from 'date-fns';
 	import Input from '$lib/components/ui/input/input.svelte';
-	import type { Category } from '$lib/types';
+	import type { Category, TimeEntry } from '$lib/types';
 	import {
 		Select,
 		SelectContent,
@@ -16,10 +21,20 @@
 		SelectLabel,
 		SelectTrigger
 	} from '$lib/components/ui/select';
+	import { Hour } from '$lib/utils';
+	import { Separator } from '$lib/components/ui/separator';
+	import {
+		Card,
+		CardContent,
+		CardDescription,
+		CardHeader,
+		CardTitle
+	} from '$lib/components/ui/card';
+	import { Badge } from '$lib/components/ui/badge';
 
 	let { data }: PageProps = $props();
 
-	const { form, constraints, enhance } = superForm(data.form, {dataType: "json"});
+	const { form, constraints, enhance } = superForm(data.form, { dataType: 'json' });
 
 	let dateValue = $derived(
 		$form.date ? fromDate(toDate($form.date), getLocalTimeZone()) : undefined
@@ -38,78 +53,126 @@
 		return res;
 	});
 
+	const daySummary = $derived.by(() => {
+		data.daySummary.timeEntries.forEach((entry) => console.log(entry.duration));
+		const grouped: Record<number, TimeEntry> = {};
+		for (const entry of data.daySummary.timeEntries) {
+			if (grouped[entry.categoryId] == undefined) {
+				grouped[entry.categoryId] = {
+					...entry
+				};
+			} else {
+				grouped[entry.categoryId].duration += entry.duration;
+			}
+		}
+
+		return {
+			...data.daySummary,
+			timeEntries: Object.values(grouped)
+		};
+	});
+
 	const categoryTriggerContent = $derived(
 		data.categories.find((c) => c.id == $form.categoryId)?.title ?? 'Vælg en kategori'
 	);
 
 	const onValueChange = (v: DateValue | undefined) => {
 		if (v) {
-			$form.date = format(v.toDate(getLocalTimeZone()), "yyyy-MM-dd");
+			$form.date = format(v.toDate(getLocalTimeZone()), 'yyyy-MM-dd');
 		} else {
 			$form.date = '';
 		}
 	};
 </script>
 
-<form 
-	method="POST" 
-	class="flex w-[350px] flex-col gap-4 rounded border p-4 shadow-sm"
-	use:enhance
->
-	<div class="grid gap-1">
-		<Label class="gap-[2px]">Dato<span class="text-red-700">*</span></Label>
-		<DatePicker value={dateValue} {onValueChange} placeholder="Vælg en dato" class="w-full" />
-	</div>
-
-	<div class="grid gap-1">
-		<Label class="gap-[2px]" for="category">Kategori<span class="text-red-700">*</span></Label>
-		<Select
-			type="single"
-			name="category"
-			value={$form.categoryId.toString()}
-			onValueChange={(id) => ($form.categoryId = parseInt(id))}
+<Card class="w-[350px]">
+	<CardHeader>
+		<CardTitle>
+			Registreringer d. {new DateFormatter('da-DK', { dateStyle: 'long' }).format(
+				new Date(daySummary.date)
+			)}
+		</CardTitle>
+		<CardDescription
+			>Timer: {daySummary.totalHours / Hour} Max timer: {daySummary.maxHours /
+				Hour}</CardDescription
 		>
-			<SelectTrigger class="w-full">
-				{categoryTriggerContent}
-			</SelectTrigger>
-			<SelectContent>
-				{#each Object.entries(categoryMap) as [label, categories]}
-					<SelectGroup>
-						<SelectLabel>{label}</SelectLabel>
-						{#each categories as category}
-							<SelectItem value={category.id.toString()}>
-								{category.title}
-							</SelectItem>
+	</CardHeader>
+	<CardContent>
+		{#each daySummary.timeEntries as entry (entry.id)}
+			<div class="mb-2 flex items-center justify-between rounded-lg border p-2">
+				<div>
+					<p class="font-semibold tracking-tight">{entry.category}</p>
+					<p class="text-muted-foreground text-sm">
+						{((entry.duration / daySummary.totalHours) * 100).toFixed(1)}% af total
+					</p>
+				</div>
+				<div>
+					<Badge variant="outline">{entry.duration / Hour}T</Badge>
+				</div>
+			</div>
+		{/each}
+	</CardContent>
+</Card>
+
+<Card class="w-[350px]">
+	<CardContent>
+		<form method="POST" class="flex flex-col gap-4" use:enhance>
+			<div class="grid gap-1">
+				<Label class="gap-[2px]">Dato<span class="text-red-700">*</span></Label>
+				<DatePicker value={dateValue} {onValueChange} placeholder="Vælg en dato" class="w-full" />
+			</div>
+
+			<div class="grid gap-1">
+				<Label class="gap-[2px]" for="category">Kategori<span class="text-red-700">*</span></Label>
+				<Select
+					type="single"
+					name="category"
+					value={$form.categoryId.toString()}
+					onValueChange={(id) => ($form.categoryId = parseInt(id))}
+				>
+					<SelectTrigger class="w-full">
+						{categoryTriggerContent}
+					</SelectTrigger>
+					<SelectContent>
+						{#each Object.entries(categoryMap) as [label, categories]}
+							<SelectGroup>
+								<SelectLabel>{label}</SelectLabel>
+								{#each categories as category}
+									<SelectItem value={category.id.toString()}>
+										{category.title}
+									</SelectItem>
+								{/each}
+							</SelectGroup>
 						{/each}
-					</SelectGroup>
-				{/each}
-			</SelectContent>
-		</Select>
-	</div>
+					</SelectContent>
+				</Select>
+			</div>
 
-	<div class="grid gap-1">
-		<Label class="gap-[2px]" for="duration">Timer<span class="text-red-700">*</span></Label>
-		<Input
-			name="duration"
-			bind:value={$form.durationHours}
-			type="number"
-			step="0.01"
-			placeholder="Indtast antal timer"
-			{...$constraints.durationHours}
-		/>
-	</div>
+			<div class="grid gap-1">
+				<Label class="gap-[2px]" for="duration">Timer<span class="text-red-700">*</span></Label>
+				<Input
+					name="duration"
+					bind:value={$form.durationHours}
+					type="number"
+					step="0.01"
+					placeholder="Indtast antal timer"
+					{...$constraints.durationHours}
+				/>
+			</div>
 
-	<div class="grid gap-1">
-		<Label for="description">Beskrivelse</Label>
-		<Input
-			name="description"
-			bind:value={$form.description}
-			placeholder="Indtast valgfri beskrivelse"
-			{...$constraints.description}
-		/>
-	</div>
+			<div class="grid gap-1">
+				<Label for="description">Beskrivelse</Label>
+				<Input
+					name="description"
+					bind:value={$form.description}
+					placeholder="Indtast valgfri beskrivelse"
+					{...$constraints.description}
+				/>
+			</div>
 
-	<Button type="submit" class="mt-2">Opret</Button>
-</form>
+			<Button type="submit" class="mt-2">Opret</Button>
+		</form>
+	</CardContent>
+</Card>
 
 <SuperDebug data={$form} />
