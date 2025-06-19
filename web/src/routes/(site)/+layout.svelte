48bce901f3ed1cd29d67f66ff0c7f81521290906
TobiasTheDanish <t.hc199811@gmail.com>
2025-06-20 00:09:10 +0200

    feat: ui for registrations

---
+++ web/src/routes/(site)/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { LayoutProps } from './$types';
+
+	const { children }: LayoutProps = $props();
+</script>
+
+<main
+	class="mx-auto flex min-h-[100vh] max-w-[1600px] flex-col items-center justify-center overflow-auto px-8"
+>
+	{@render children()}
+</main>
