36fcec1f18ba77d1b501d9386fa32e3be5ae9976
Tobias <t.hc199811@gmail.com>
2025-06-25 10:48:06 +0200

    fix: add secure and http-only to authToken cookie

--- web/src/routes/auth/login/+page.server.ts
+++ web/src/routes/auth/login/+page.server.ts
@@ -27,7 +27,12 @@ export const actions: Actions = {
 			return fail(500, { form, error: loginRes.error });
 		}
 
-		cookies.set('authToken', loginRes.data.token, { path: '/', maxAge: (7 * 24 * 60 * 60) });
+		cookies.set('authToken', loginRes.data.token, {
+			path: '/',
+			maxAge: 7 * 24 * 60 * 60,
+			secure: true,
+			httpOnly: true
+		});
 
 		redirect(303, '/calendar');
 	}
