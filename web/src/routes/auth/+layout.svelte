b20d120ac4a94a9934673c2cc41db013ea88c9f7
Tobias <t.hc199811@gmail.com>
2025-06-27 14:13:46 +0200

    feat: update authentication pages

--- web/src/routes/auth/+layout.svelte
+++ web/src/routes/auth/+layout.svelte
@@ -1,9 +1,76 @@
 <script lang="ts">
+	import ProgressContent from '$lib/components/progress-content.svelte';
+	import { Hour, maxFractionDigits, toDurationString } from '$lib/utils';
+	import { onMount } from 'svelte';
 	import type { LayoutProps } from './$types';
+	import { Separator } from '$lib/components/ui/separator';
+
+	const entries = [2 * Hour, 1.5 * Hour, 1 * Hour, 1.5 * Hour, 2 * Hour];
 
 	const { children }: LayoutProps = $props();
+	const usePercent = false;
+	let totalHours = $state(0 * Hour);
+	let entryCount = $state(0);
+	const maxHours = 8 * Hour;
+
+	const totalHoursStr = $derived(toDurationString(totalHours));
+	const maxHoursStr = $derived(toDurationString(maxHours));
+	const remainingHours = $derived(maxHours - totalHours);
+	const averageTime = $derived(
+		entryCount && totalHours ? maxFractionDigits(totalHours / entryCount, 2) : 0
+	);
+
+	const completedPercentage = $derived(maxFractionDigits((totalHours / maxHours) * 100, 2));
+	const averagePercentage = $derived(maxFractionDigits(completedPercentage / entryCount, 2));
+	const progressPercentage = $derived(
+		maxFractionDigits(Math.min((totalHours / maxHours) * 100, 100), 2)
+	);
+
+	const timeoutCallback = (count = 0) => {
+		if (count < entries.length) {
+			totalHours += entries[count++];
+			entryCount = count;
+			setTimeout(() => timeoutCallback(count), 2500);
+		}
+	};
+
+	onMount(() => {
+		setTimeout(timeoutCallback, 1000);
+	});
 </script>
 
-<main class="flex h-[100vh] max-w-[1600px] flex-col items-center justify-center overflow-auto mx-auto">
-	{@render children()}
+<main class="relative grid min-h-svh lg:grid-cols-2">
+	<div class="bg-muted hidden lg:grid lg:items-center lg:justify-center">
+		<div
+			class="bg-background grid items-center justify-center justify-items-center gap-12 rounded-xl px-12 py-8"
+		>
+			<div class="grid w-[40ch] gap-4 pt-4 text-center">
+				<h1
+					class="text-muted-foreground text-center text-2xl leading-none font-bold tracking-tight"
+				>
+					Velkommen til SkanCode Tid
+				</h1>
+				<p class="text-foreground/45 text-center text-sm leading-none">
+					Simpel, nem og hurtig tidsregistrering
+				</p>
+			</div>
+			<Separator />
+			<div class="flex flex-col items-center gap-8">
+				<ProgressContent
+					{usePercent}
+					{completedPercentage}
+					{progressPercentage}
+					{averagePercentage}
+					{averageTime}
+					{remainingHours}
+					{totalHoursStr}
+					{maxHoursStr}
+					{entryCount}
+				/>
+			</div>
+		</div>
+	</div>
+	<div class="m-auto flex flex-col p-6 md:p-10">
+		{@render children()}
+	</div>
 </main>
--- web/src/routes/auth/login/+page.svelte
+++ web/src/routes/auth/login/+page.svelte
@@ -10,14 +10,26 @@
 	const { form } = superForm(data.form);
 </script>
 
-<form method="POST" class="flex w-[350px] flex-col gap-3 rounded border p-4 shadow-sm">
-	<div class="flex flex-col gap-1">
-		<Label for="email">Email</Label>
-		<Input type="email" name="email" bind:value={$form.email} />
+<form method="POST" class="flex w-[350px] flex-col gap-6">
+	<div class="flex flex-col items-center gap-2 text-center">
+		<h1 class="text-2xl font-bold">Log ind på din konto</h1>
+		<p class="text-muted-foreground text-sm text-balance">
+			Indtast din email og adgangskode her for at logge ind
+		</p>
 	</div>
-	<div class="flex flex-col gap-1">
-		<Label for="password">Adgangskode</Label>
-		<Input type="password" name="password" bind:value={$form.password} />
+	<div class="grid gap-6">
+		<div class="flex flex-col gap-2">
+			<Label for="email">Email</Label>
+			<Input type="email" name="email" bind:value={$form.email} />
+		</div>
+		<div class="flex flex-col gap-2">
+			<Label for="password">Adgangskode</Label>
+			<Input type="password" name="password" bind:value={$form.password} />
+		</div>
+		<Button type="submit">Log ind</Button>
 	</div>
-	<Button type="submit" class="mt-2">Log ind</Button>
+	<div class="text-center text-sm">
+		Har du ikke en konto?
+		<a href="/auth/registrer" class="underline underline-offset-4"> Opret dig her </a>
+	</div>
 </form>
